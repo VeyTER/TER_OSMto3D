56c64137281544c4f577ea12012490eee01567d1
Dorian Roques <dorian-roques@orange.fr>
2017-04-25 22:05:02 +0200

    Complément du dernier commit

---
+++ Assets/Scripts/UINames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class UINames {
+	public const string BUILDING_NODES_BUTTON = "BuildingNodesButton";
+	public const string HIGHWAY_NODES_BUTTON = "HighwayNodesButton";
+	public const string WALLS_BUTTON = "WallsButton";
+	public const string ROOFS_BUTTON = "RoofsButton";
+	public const string HIGHWAYS_BUTTON = "HighwaysButton";
+	public const string FOOTWAYS_BUTTON = "FootwaysButton";
+	public const string CYCLEWAYS_BUTTON = "CyclewaysButton";
+	public const string TREES_BUTTON = "TreesButton";
+
+	public const string INFOS_PANEL_CLOSE_BUTTON = "InfoPanelCloseButton";
+	public const string TEST_BUTTON = "TestButton";
+
+	public const string INFO_PANEL = "InfoPanel";
+}
