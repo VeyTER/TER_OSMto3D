d44628c13212d0a5a4bece895c42b709ca2bb0c6
Dorian Roques <dorian-roques@orange.fr>
2017-05-15 17:24:36 +0200

    Ajout des commentaires pour toutes les classes contenant les constantes.

--- Assets/Scripts/constants/ObjectNames.cs
+++ Assets/Scripts/constants/ObjectNames.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 
+/// <summary>
+/// 	Contient le nom des objects d'une ville.
+/// </summary>
 public class ObjectNames {
+	/// <summary> Nom de la ville. </summary>
 	public const string CITY = "City";
+
+	/// <summary> Nom des noeuds de bâtiments. </summary>
 	public const string BUILDING_NODES = "Buildings nodes";
+
+	/// <summary> Nom des noeuds de routes. </summary>
 	public const string HIGHWAY_NODES = "Highway nodes";
+
+	/// <summary> Nom des bâtiments. </summary>
 	public const string WALLS = "Walls groups (buildings)";
+
+	/// <summary> Nom des toîts. </summary>
 	public const string ROOFS = "Roofs";
+
+	/// <summary> Nom des routes. </summary>
 	public const string HIGHWAYS = "Highways";
+
+	/// <summary> Nom des chemins piétons. </summary>
 	public const string FOOTWAYS = "Footways";
+
+	/// <summary> Nom des pistes cyclables. </summary>
 	public const string CYCLEWAYS = "Cycleways";
+
+	/// <summary> Nom des villes. </summary>
 	public const string TREES = "Trees";
 }
